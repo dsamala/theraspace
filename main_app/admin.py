3b2e7229266f0e93ecd316c0b92fb2d5bd6545d8
Denzel <denzelsamala@Denzel-2.local>
2022-04-01 21:24:39 -0700

    initial login-signup structure, django authform pending debug

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Patient
+from .models import Clinician
 
 # Register your models here.
+admin.site.register(Clinician)
+admin.site.register(Patient)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from .models import Patient, Clinician
+from django.contrib.auth.models import User
+from main_app.models import Patient, Clinician
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
+
+class Home(TemplateView):
+    template_name='home.html'
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    print('The account does not exist or has been disabled.')
+            else:
+                print('The username and/or password is incorrect.')
+                return HttpResponseRedirect('/login')
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/login')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            HttpResponse('<h1>Please Try Again</h1>')
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
